b1e3f9b4fc0b496244e5af61e982511a41dcd937
Prakriti <prakritijanbandhu@gmail.com>
2024-12-02 16:21:09 +0530

    Added trial data

---
+++ Example_usage.jl
@@ -0,0 +1,13 @@
+using .BasicPCA
+using Plots
+
+path = "TrialData.csv"
+delim = ' '
+dims = 2
+df = get_numeric_df(path, delim)
+A = get_transform_matr(df, dims)
+red_df = converted_df(df, A)
+print(red_df)
+
+scatter(df[!,1], df[!,2], df[!,3], title="Original Data")
+scatter(red_df[!,1], red_df[!,2], title="Reduced Data")
